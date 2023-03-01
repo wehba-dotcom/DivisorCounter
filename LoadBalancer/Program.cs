91bd0ef49004eb01b3a376b3cd5acfcb2861ed1b
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2023-03-01 12:35:00 +0100

    Added load balancer and updated documentation

--- LoadBalancer/Program.cs
+++ LoadBalancer/Program.cs
@@ -1,7 +1,3 @@
-using System.Net;
-
-Console.WriteLine("Hostname: " + Environment.MachineName);
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,7 +10,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
